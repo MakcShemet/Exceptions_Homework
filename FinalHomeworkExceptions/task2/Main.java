279234139ab658b158ceae0698ddf11603ed1d8d
DevMasterSh <dev_master_sh@mail.ru>
2023-08-17 17:54:01 +0300

    Продвинутая работа с исключениями в Java

--- FinalHomeworkExceptions/task2/Main.java
+++ FinalHomeworkExceptions/task2/Main.java
@@ -8,45 +8,15 @@ import task2.Exceptions.FileIsExists;
 import task2.Exceptions.FileNotFoundException;
 import task2.Exceptions.WriterStringIsEmpty;
 import task2.FileManager.FileManager;
+import task2.Menu.Menu;
 
 public class Main {
     public static void main(String[] args)
             throws FileNotFoundException, WriterStringIsEmpty, FileIsEmpty, FileIsExists, IOException {
-        Boolean flag = true;
-        Scanner iScanner = new Scanner(System.in);
-        String menu = "\nВыберите необходимый пункт меню: \n1 - чтение файла;\n2 - запись в файл;\n3 - копирование файла;\n0 - выход из программы";
 
-        while (flag) {
-            System.out.println(menu);
-            System.out.println();
-            String input = iScanner.nextLine();
-            switch (input) {
-                case "1":
-                    System.out.print("Введите имя файла для чтения: ");
-                    String filename = iScanner.nextLine();
-                    FileManager fileManager = new FileManager(filename);
-                    fileManager.fileManagerReader();
-                    break;
-                case "2":
-                    System.out.print("Введите имя файла для записи: ");
-                    filename = iScanner.nextLine();
-                    fileManager = new FileManager(filename);
-                    fileManager.fileManagerWriter();
-                    break;
-                case "3":
-                    System.out.print("Введите имя файла для копирования: ");
-                    filename = iScanner.nextLine();
-                    fileManager = new FileManager(filename);
-                    fileManager.fileManagerCreateCopy();
-                    break;
-                case "0":
-                    System.out.println("Выполнение программы завершено");
-                    flag = false;
-                    break;
-                default:
-                    System.out.println("Введите корректный пункт меню");
-            }
-        }
-        iScanner.close();
+        Menu menu = new Menu();
+
+            menu.getMenu();
+        
     }
 }
---
+++ FinalHomeworkExceptions/task2/Menu/Menu.java
@@ -0,0 +1,56 @@
+﻿package task2.Menu;
+
+import java.util.Scanner;
+
+import task2.FileManager.FileManager;
+
+public class Menu {
+
+    public Menu() {
+
+    }
+
+    boolean flag = true;
+
+    public void getMenu() {
+
+        String menu = "\nВыберите необходимый пункт меню: \n1 - чтение файла;\n2 - запись в файл;\n3 - копирование файла;\n0 - выход из программы";
+
+        try (Scanner iScanner = new Scanner(System.in)) {
+            while (flag) {
+                System.out.println(menu);
+                System.out.println();
+                String input = iScanner.nextLine();
+                switch (input) {
+                    case "1":
+                        System.out.print("Введите имя файла для чтения: ");
+                        String filename = iScanner.nextLine();
+                        FileManager fileManager = new FileManager(filename);
+                        fileManager.fileManagerReader();
+                        break;
+                    case "2":
+                        System.out.print("Введите имя файла для записи: ");
+                        filename = iScanner.nextLine();
+                        fileManager = new FileManager(filename);
+                        fileManager.fileManagerWriter();
+                        break;
+                    case "3":
+                        System.out.print("Введите имя файла для копирования: ");
+                        filename = iScanner.nextLine();
+                        fileManager = new FileManager(filename);
+                        fileManager.fileManagerCreateCopy();
+                        break;
+                    case "0":
+                        System.out.println("Выполнение программы завершено");
+                        flag = false;
+                        break;
+                    default:
+                        System.out.println("Введите корректный пункт меню");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
